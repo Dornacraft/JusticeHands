5573b4bfdbced5852755d575492c3a9e5e1c31ad
Glynix <gwenael.lavigne@outlook.fr>
2020-04-18 21:21:03 +0200

    Adding method to get a sanction by ID, formated or not

--- JusticeHands/src/fr/dornacraft/justicehands/sql/SqlSanctionManager.java
+++ JusticeHands/src/fr/dornacraft/justicehands/sql/SqlSanctionManager.java
@@ -12,8 +12,10 @@ import java.util.UUID;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
+import fr.dornacraft.justicehands.GeneralUtils;
 import fr.dornacraft.justicehands.Main;
 import fr.dornacraft.justicehands.criminalrecords.objects.CJSanction;
+import fr.dornacraft.justicehands.sanctionmanager.SanctionCancel;
 import fr.dornacraft.justicehands.sanctionmanager.objects.Sanction;
 
 public class SqlSanctionManager {
@@ -52,6 +54,11 @@ public class SqlSanctionManager {
 			rs.setString(2, target.getUniqueId().toString());
 			rs.executeUpdate();
 			rs.close();
+			
+			// Il faut récupérer la sanction générée antérieurement pour l'ajouter dans
+			// la hashmap pour que si le modérateur ai l'envie, de pouvoir l'annuler via l'ID
+			setModLastSanctionOnMap(moderator);
+			
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
@@ -86,5 +93,82 @@ public class SqlSanctionManager {
 			e.printStackTrace();
 		}
 		return null;
+	}
+	
+	private void setModLastSanctionOnMap(Player moderator) {
+		CJSanction sanction = new CJSanction();
+		try {
+			PreparedStatement q = connection.prepareStatement("SELECT uuid FROM sanctions_list WHERE uuid = ? ORDER BY id DESC");
+			q.setString(1, moderator.getUniqueId().toString());
+
+			ResultSet rs = q.executeQuery();
+			if (rs.next()) {
+				sanction.setID("#" + String.format("%05d", (rs.getInt("id"))));
+				sanction.setPlayer(Bukkit.getPlayer(UUID.fromString(rs.getString("uuid"))));
+				sanction.setName(rs.getString("name"));
+				sanction.setReason(rs.getString("reason"));
+				sanction.setPoints(rs.getInt("points"));
+				sanction.setTSDate(rs.getTimestamp("date"));
+				sanction.setTSExpireDate(rs.getTimestamp("expiredate"));
+				sanction.setModerator(Bukkit.getPlayer(UUID.fromString(rs.getString("moderator"))));
+				sanction.setInitialType(rs.getString("type"));
+				sanction.setState(rs.getString("state"));
+				q.close();
+				SanctionCancel.putPossibilityToCancel(sanction);
+				return;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return;
+	}
+	
+	private void getSanction(Player moderator, String sanctionStringID) {
+		CJSanction sanction = new CJSanction();
+		StringBuilder strBuilder = new StringBuilder(sanctionStringID);
+		int sanctionID = -1;
+
+		for (int i = 0; i < strBuilder.length(); i++) {
+			if (strBuilder.charAt(i) == '#' || strBuilder.charAt(i) == '0') {
+				strBuilder.setCharAt(i, '-');
+			} else {
+				break;
+			}
+		}
+		
+		try {
+			sanctionID = Integer.parseInt(strBuilder.toString());
+		} catch (Exception e) {
+			moderator.sendMessage(GeneralUtils.getPrefix("SM") + "§cLe numéro de sanction indiqué n'est pas bon.");
+		}
+		
+		try {
+			PreparedStatement q = connection.prepareStatement("SELECT * FROM sanctions_list WHERE id = ? ORDER BY id DESC");
+			if (sanctionID == -1) {
+				q.close();
+				return;
+			}
+			q.setInt(1, sanctionID);
+
+			ResultSet rs = q.executeQuery();
+			if (rs.next()) {
+				sanction.setID("#" + String.format("%05d", (rs.getInt("id"))));
+				sanction.setPlayer(Bukkit.getPlayer(UUID.fromString(rs.getString("uuid"))));
+				sanction.setName(rs.getString("name"));
+				sanction.setReason(rs.getString("reason"));
+				sanction.setPoints(rs.getInt("points"));
+				sanction.setTSDate(rs.getTimestamp("date"));
+				sanction.setTSExpireDate(rs.getTimestamp("expiredate"));
+				sanction.setModerator(Bukkit.getPlayer(UUID.fromString(rs.getString("moderator"))));
+				sanction.setInitialType(rs.getString("type"));
+				sanction.setState(rs.getString("state"));
+				q.close();
+				SanctionCancel.putPossibilityToCancel(sanction);
+				return;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return;
 	}
 }
